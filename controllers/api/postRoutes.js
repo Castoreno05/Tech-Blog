19b0a5002cd1d30883c5a9765a918cdc51cc9be7
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-08-15 22:00:28 -0500

    Created a function that makes a post request to the Posts model.

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,27 +1,26 @@
-const router = require('express').Router();
+const router = require("express").Router();
 // Bring in Posts models
-const Posts = require('../../models/Posts');
+const Posts = require("../../models/Posts");
 
 // POST route to create the new posts from dashboard
-router.post("/posts", (req, res) => {
-    Posts.create({
-        title: req.body.title,
-        content: req.body.content
-    })
+router.post("/", (req, res) => {
+  Posts.create({
+    title: req.body.title,
+    content: req.body.content,
+  })
     .then((data) => {
-        res.json(data);
+      res.json(data);
     })
-    .catch((err) =>{
-        res.json(err);
+    .catch((err) => {
+      res.sendStatus(500).send("Something has went wrong");
     });
 });
 
 // Get route to return all the post data
 router.get("/", (req, res) => {
-    Posts.findAll().then(data => {
-        res.json(data)
-    });
+  Posts.findAll().then((data) => {
+    res.json(data);
+  });
 });
-
 
 module.exports = router;
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -12,13 +12,34 @@ newPost.addEventListener("click", () => {
 
 // Remove child (section) when create has been clicked
 create.addEventListener("click", () => {
-    var sectionThree = document.getElementById("section-three");
-    var title = document.getElementById("title");
-    var content = document.getElementById("content");
-    // Remove the text values
-    if (title.value && content.value != "");
-    title.value = "";
-    content.value = "";
-    // Remove the display
-    sectionThree.setAttribute("style", "display:none;")
-})
+  var sectionThree = document.getElementById("section-three");
+  var newTitle = document.getElementById("title");
+  var newContent = document.getElementById("content");
+
+  // Place values into variables to use on the post
+  var title = newTitle.value;
+  var content = newContent.value;
+  var newPost = { title, content };
+  console.log(newPost)
+
+  // Create a variable for post url
+  var postApi = `/api/posts`;
+  // Create POST request to the Posts model
+  var options = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(newPost),
+  };
+  fetch(postApi, options);
+  // console.log(newPost);
+
+  // Remove the text values
+  if (newTitle.value && newContent.value != "");
+  newTitle.value = "";
+  newContent.value = "";
+  // Remove the display
+  sectionThree.setAttribute("style", "display:none;");
+
+});
