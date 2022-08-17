28d1d40d80ec55f1fbd7ad23329d8d1bae846841
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-08-16 21:45:32 -0500

    Created a function that grabs the existing posts and append them to an paragraph tag wrapped in achor tags. Added styling to the created posts.

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,15 +2,50 @@
 var newPost = document.getElementById("newPost");
 // Grab Create button
 var create = document.getElementById("create");
+// Create a body variable
+var body = document.body.children[1];
+// console.log(body);
 
-// When a new post is selected. Show newPost form.
-newPost.addEventListener("click", () => {
-  var sectionThree = document.getElementById("section-three");
-  // Remove the display for section-three
-  sectionThree.setAttribute("style", "");
-});
+// Need to click the links to call content
+var btns = document.querySelectorAll(".btnLinks");
+var btnsArr = Array.from(btns);
+console.log(btnsArr);
+
+createdPost();
+function createdPost() {
+  // Create a variable for the url to live
+  var post = `/api/posts`;
+  // Make a fetch request to obtain createdPosts
+  fetch(post)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      for (var i = 0; i < data.length; i++) {
+        var titles = data[i].title;
+        var creation = data[i].createdAt;
+        var newCreation = creation.slice(0, creation.length - 14);
+        // console.log(titles, newCreation);
+
+        // Create element to populate the body
+        var dashboardPost = document.createElement("p");
+        // Create link around the element
+        var anchorTag = document.createElement("a");
+
+        dashboardPost.textContent = titles;
+        // Set attributes to the appended elements
+        dashboardPost.setAttribute("class", "postBtn");
+        anchorTag.setAttribute("class", "btnLinks");
+
+        // Append a button to the body
+        body.appendChild(anchorTag).appendChild(dashboardPost);
+      }
+    });
+}
 
 // Remove child (section) when create has been clicked
+// Post results from title and content inputs
+// Reset input values
 create.addEventListener("click", () => {
   var sectionThree = document.getElementById("section-three");
   var newTitle = document.getElementById("title");
@@ -20,7 +55,7 @@ create.addEventListener("click", () => {
   var title = newTitle.value;
   var content = newContent.value;
   var newPost = { title, content };
-  console.log(newPost)
+  console.log(newPost);
 
   // Create a variable for post url
   var postApi = `/api/posts`;
@@ -42,4 +77,12 @@ create.addEventListener("click", () => {
   // Remove the display
   sectionThree.setAttribute("style", "display:none;");
 
+  location.reload();
+});
+
+// When a new post is selected. Show newPost form.
+newPost.addEventListener("click", () => {
+  var sectionThree = document.getElementById("section-three");
+  // Remove the display for section-three
+  sectionThree.setAttribute("style", "");
 });
