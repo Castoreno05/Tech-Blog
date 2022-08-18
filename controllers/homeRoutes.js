aff44562950c34941dd04aa75a31755faa46dc7e
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-08-18 12:29:21 -0500

    Added a route that will return all the posts in the database then render those posts into the home.handlebars using custom helpers.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,23 +1,33 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const Posts = require("../models/Posts");
 
 // Render login.handlebars
 router.get("/", (req, res) => {
-    res.render('login');
+  res.render("login");
 });
 
-// Render sign-up.handlebars 
+// Render sign-up.handlebars
 router.get("/sign-up", (req, res) => {
-    res.render('sign-up');
-})
+  res.render("sign-up");
+});
 
 // Render home.handlebars
-router.get('/home', (req, res) => {
-    res.render("home");
+// router.get('/home', (req, res) => {
+//     res.render("home");
+// });
+
+// Get route to return all the post data then render home.handlebars
+router.get("/home", async (req, res) => {
+  const allPostData = await Posts.findAll();
+  const posts = allPostData.map((p) => {
+    return p.get({ plain: true });
+  });
+  res.render("home", { posts: posts });
 });
 
 // Render dashboard
-router.get('/dashboard', (req, res) => {
-    res.render("dashboard");
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard");
 });
 
 module.exports = router;
