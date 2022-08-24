02e983e8cb381d638d5c684e659cfcbb9070e7c9
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-08-24 09:15:21 -0500

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,29 @@
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PW,
+//     {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306,
+//       logging: false
+//     }
+//   );
+  
+//   module.exports = sequelize;
+
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PW,
@@ -12,5 +34,6 @@ const sequelize = new Sequelize(
       logging: false
     }
   );
-  
-  module.exports = sequelize;
+}
+
+module.exports = sequelize;
